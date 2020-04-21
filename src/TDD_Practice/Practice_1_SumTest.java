539a2eef15d932954eb4198c5ea00fc58fc57927
hwk0911@gmail.com <hwk0911@gmail.com>
2020-04-21 18:12:54 +0900

    Practice_1_SumTest.java
    TDD의 연습을 위해 두 정수를 매개변수로 하는 함수 getSum을 작성했다.

---
+++ src/TDD_Practice/Practice_1_SumTest.java
@@ -0,0 +1,13 @@
+package TDD_Practice;
+
+public class Practice_1_SumTest {
+    public static void main(String[] args) {
+        //두 정수가 주어질 때, 합한 결과를 반환하는 함수를 작성하고, 테스트 코드를 작성하시오.
+
+
+    }
+
+    public int getSum (int a, int b) {
+        return a + b;
+    }
+}
