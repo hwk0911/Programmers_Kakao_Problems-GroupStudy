6db8f530f500bd769556c618900f2e8c931bb0b3
hwk0911@gmail.com <hwk0911@gmail.com>
2020-04-21 18:11:59 +0900

    Practice_1_SumTestTest.java
    TDD의 기본
    
    Junit5 + hamcrest2.2 를 통한 테스트코드 작성
    두 정수가 주어질 때, 두 정수를 합한 값을 리턴하는 함수의 테스트 코드.

---
+++ Test/TDD_Practice/Practice_1_SumTestTest.java
@@ -0,0 +1,37 @@
+package TDD_Practice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+class Practice_1_SumTestTest {
+
+    Practice_1_SumTest p1s = new Practice_1_SumTest();
+
+    @Test
+    void getSum() {
+        int a = 1;
+        int b = 5;
+        int answer = 8;
+
+        try {
+            assertThat(answer, is(p1s.getSum(a, b)));
+        }
+        catch (AssertionError e) {
+            System.out.println(a + " + " + b + " = " + "NOT " + answer);
+        }
+
+        a = 3;
+        b = 5;
+        answer = 8;
+
+        try {
+            assertThat(answer, is(p1s.getSum(a, b)));
+        }
+        catch (AssertionError e) {
+            System.out.println(a + " + " + b + " = " + "NOT " + answer);
+        }
+    }
+}
